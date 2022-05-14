28a4d0c2fbccc1dda7d3ae83b5df2b13db0a53c9
Yura Synkulych <synkulych.yura@gmail.com>
2022-05-14 20:31:53 +0300

    Refactored MysqlColorDAO.java

--- src/main/java/ua/synkulych/sort_it/dao/entity/Color.java
+++ src/main/java/ua/synkulych/sort_it/dao/entity/Color.java
@@ -3,11 +3,24 @@ package ua.synkulych.sort_it.dao.entity;
 public class Color {
 
   private String value;
+  private int id;
 
   public Color() {}
 
   public Color (String value) {
     this.value = value;
+  }
+  public Color (int id, String value) {
+    this.value = value;
+    this.id = id;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
   }
 
   public String getValue() {
--- src/main/java/ua/synkulych/sort_it/dao/impl/mysql/MysqlColorDAO.java
+++ src/main/java/ua/synkulych/sort_it/dao/impl/mysql/MysqlColorDAO.java
@@ -1,6 +1,6 @@
 package ua.synkulych.sort_it.dao.impl.mysql;
 
-import ua.synkulych.sort_it.dao.abstraction.ColorDAO;
+import ua.synkulych.sort_it.dao.DAO;
 import ua.synkulych.sort_it.dao.entity.Color;
 import ua.synkulych.sort_it.dao.exception.DAOException;
 import ua.synkulych.sort_it.dao.utils.ConnectionManager;
@@ -9,13 +9,15 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
-public class MysqlColorDAO extends ColorDAO {
+public class MysqlColorDAO implements DAO<Color, Integer> {
 
-  private static final String GET_COLOR_BY_ID = """
-      SELECT  colors.value
+  private static final String GET_ALL_COLORS = """
+      SELECT  *
         FROM  colors
-       WHERE  colors.id=?
       """;
 
   private static final String UPDATE_COLOR_BY_ID = """
@@ -24,10 +26,10 @@ public class MysqlColorDAO extends ColorDAO {
        WHERE  colors.id=?
       """;
 
-  private static final String DELETE_COLOR_BY_VALUE = """
+  private static final String DELETE_COLOR_BY_ID = """
       DELETE
         FROM  colors
-       WHERE  colors.value=?
+       WHERE  colors.id=?
       """;
 
   private static final String INSERT_NEW_COLOR = """
@@ -36,8 +38,71 @@ public class MysqlColorDAO extends ColorDAO {
        VALUE  (NULL, ?)
       """;
 
+  @Override
+  public Optional<Color> get(Integer integer) {
+    return Optional.empty();
+  }
+
+  @Override
+  public List<Color> getAll() {
+
+    try (Connection connection = ConnectionManager.openConnection();
+         PreparedStatement statement = connection.prepareStatement(GET_ALL_COLORS)) {
+      ResultSet result = statement.executeQuery();
+      ArrayList<Color> colorList = new ArrayList<>();
+      while (result.next()) {
+        colorList.add(new Color(result.getInt("id"), result.getString("value")));
+      }
+      return colorList;
+    } catch (SQLException e) {
+      throw new DAOException(e);
+    }
+  }
+
+  @Override
+  public Color save(Color color) {
+
+    try (Connection connection = ConnectionManager.openConnection();
+         PreparedStatement statement = connection.prepareStatement(INSERT_NEW_COLOR)) {
+      statement.setString(1, color.getValue());
+      ResultSet result = statement.executeQuery();
+      if (result.next()) return color;
+      throw new DAOException("Can not add color to the database");
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public boolean update(Color color) {
+
+    try (Connection connection = ConnectionManager.openConnection();
+         PreparedStatement statement = connection.prepareStatement(UPDATE_COLOR_BY_ID)) {
+      statement.setInt(1, color.getId());
+      statement.setString(1, color.getValue());
+      ResultSet result = statement.executeQuery();
+      return result.next();
+    } catch (SQLException e) {
+      throw new DAOException(e);
+    }
+  }
+
+  @Override
+  public boolean delete(Integer id) {
+
+    try (Connection connection = ConnectionManager.openConnection();
+         PreparedStatement statement = connection.prepareStatement(DELETE_COLOR_BY_ID)) {
+      statement.setInt(1, id);
+      ResultSet result = statement.executeQuery();
+      return result.next();
+    } catch (SQLException e) {
+      throw new DAOException(e);
+    }
+  }
+
   private MysqlColorDAO() {
   }
+
 
   private static class SingletonHolder {
     private static final MysqlColorDAO INSTANCE = new MysqlColorDAO();
@@ -45,58 +110,5 @@ public class MysqlColorDAO extends ColorDAO {
 
   public static MysqlColorDAO getInstance() {
     return MysqlColorDAO.SingletonHolder.INSTANCE;
-  }
-
-  @Override
-  public Color getColorById(int id) {
-
-    try (Connection connection = ConnectionManager.openConnection();
-         PreparedStatement statement = connection.prepareStatement(GET_COLOR_BY_ID)) {
-      statement.setInt(1, id);
-      ResultSet result = statement.executeQuery();
-      result.next();
-      return new Color(result.getNString("value"));
-    } catch (SQLException e) {
-      throw new DAOException(e);
-    }
-  }
-
-  @Override
-  public boolean updateColorById(int id) {
-
-    try (Connection connection = ConnectionManager.openConnection();
-         PreparedStatement statement = connection.prepareStatement(UPDATE_COLOR_BY_ID)) {
-      statement.setInt(1, id);
-      ResultSet result = statement.executeQuery();
-      return result.next();
-    } catch (SQLException e) {
-      throw new DAOException(e);
-    }
-  }
-
-  @Override
-  public boolean deleteColorById(String value) {
-
-    try (Connection connection = ConnectionManager.openConnection();
-         PreparedStatement statement = connection.prepareStatement(DELETE_COLOR_BY_VALUE)) {
-      statement.setString(1, value);
-      ResultSet result = statement.executeQuery();
-      return result.next();
-    } catch (SQLException e) {
-      throw new DAOException(e);
-    }
-  }
-
-  @Override
-  public boolean addColor(String value) {
-
-    try (Connection connection = ConnectionManager.openConnection();
-         PreparedStatement statement = connection.prepareStatement(INSERT_NEW_COLOR)) {
-      statement.setString(1, value);
-      ResultSet result = statement.executeQuery();
-      return result.next();
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
-    }
   }
 }
